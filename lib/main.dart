184955a52fca29ab3b442ff425a7fc4757de3048
Ahmed Abdullah <125463647+ahmedabdalla67@users.noreply.github.com>
2024-09-19 18:48:26 +0300

    add package go_router

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'package:mesk/core/database/cache/cache_helper.dart';
 import 'package:mesk/core/services/service_locator.dart';
 import 'package:mesk/core/utils/app_data.dart';
 import 'package:mesk/core/utils/bloc_observer.dart';
+import 'package:mesk/core/utils/routes.dart';
 import 'package:mesk/core/utils/theme_manager.dart';
-import 'package:mesk/features/main_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,8 +21,8 @@ class Mesk extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const MainScreen(),
+    return MaterialApp.router(
+      routerConfig: router,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(textTheme: getApplicationTheme().textTheme),
     );
