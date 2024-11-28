79380b7e6d1db6544231aebf07bd0f1f3955012f
ahmedabdalla67 <ahmedabdalla6728@gmail.com>
2024-11-28 16:31:17 +0200

    edit on hadith entity

--- lib/features/hadith/domain_layer/entities/hadith_entity.dart
+++ lib/features/hadith/domain_layer/entities/hadith_entity.dart
@@ -1,14 +1,11 @@
 class HadithEntity {
-  final String hadith;
+  final String hadithText;
   final int number;
   final String? description;
-  final String url;
-  final bool isDownloaded;
 
-  HadithEntity(
-      {required this.url,
-      required this.isDownloaded,
-      required this.hadith,
-      required this.number,
-      required this.description});
+  HadithEntity({
+    required this.description,
+    required this.hadithText,
+    required this.number,
+  });
 }
