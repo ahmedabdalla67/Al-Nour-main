479df5f6bc375e01ae89e1d767839d48a1562888
ahmedabdalla67 <ahmedabdalla6728@gmail.com>
2024-11-28 16:28:12 +0200

    delete the prev model for hadith and create this one

---
+++ lib/features/hadith/data_layer/models/hadith_content_model.dart
@@ -0,0 +1,36 @@
+import 'package:mesk/features/hadith/domain_layer/entities/hadith_entity.dart';
+
+class HadithModel {
+  final String hadithText;
+  final String? description;
+  final int number;
+  final String searchTerm;
+
+  HadithModel(
+      {required this.hadithText,
+      required this.description,
+      required this.number,
+      required this.searchTerm});
+
+  factory HadithModel.fromJson(Map<String, dynamic> json) {
+    return HadithModel(
+        hadithText: json['hadith'],
+        description: json['description'] ?? '',
+        number: json['number'],
+        searchTerm: json['searchTerm']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'hadith': hadithText,
+      'description': description ?? '',
+      'number': number,
+      'seachTerm': searchTerm,
+    };
+  }
+
+  HadithEntity toEntity() {
+    return HadithEntity(
+        description: description ?? '', hadithText: hadithText, number: number);
+  }
+}
