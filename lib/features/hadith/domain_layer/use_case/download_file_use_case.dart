d4824dd8c47668f91e721b133f5c392d84d2b23b
ahmedabdalla67 <ahmedabdalla6728@gmail.com>
2025-03-15 04:36:04 +0200

    edite dowloadFileUseCase file to change the data type of cachHadith and in cubit create initiale method to detect downloadStatus and edite on toggle Method

--- lib/features/hadith/domain_layer/use_case/download_file_use_case.dart
+++ lib/features/hadith/domain_layer/use_case/download_file_use_case.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:mesk/core/errors/failure.dart';
+import 'package:mesk/features/hadith/domain_layer/entities/hadith_entity.dart';
 import 'package:mesk/features/hadith/domain_layer/repository/hadith_repo.dart';
 
 class DownloadFileUseCase {
@@ -7,7 +8,9 @@ class DownloadFileUseCase {
 
   DownloadFileUseCase({required this.hadithRepo});
 
-  Future<Either<Failure, void>> call(String fileName, String content) async {
+  Future<Either<Failure, void>> call(
+      String fileName, List<HadithEntity> content) async {
+    print('DownloadFileUseCase__');
     return await hadithRepo.cacheHadith(fileName, content);
   }
 }
--- lib/features/hadith/presentation/cubit/hadith_cubit.dart
+++ lib/features/hadith/presentation/cubit/hadith_cubit.dart
@@ -1,11 +1,13 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:mesk/core/services/service_locator.dart';
 import 'package:mesk/features/hadith/domain_layer/entities/hadith_entity.dart';
+import 'package:mesk/features/hadith/domain_layer/repository/hadith_repo.dart';
 import 'package:mesk/features/hadith/domain_layer/use_case/check_hadith_status_use_case.dart';
 import 'package:mesk/features/hadith/domain_layer/use_case/download_file_use_case.dart';
 import 'package:mesk/features/hadith/domain_layer/use_case/get_hadith_files_use_case.dart';
 import 'package:mesk/features/hadith/domain_layer/use_case/remove_hadith_use_case.dart';
-import 'package:meta/meta.dart';
+import 'package:mesk/features/hadith/presentation/screens/hadith_content_Screen.dart';
 
 part 'hadith_state.dart';
 
@@ -22,7 +24,22 @@ class HadithCubitCubit extends Cubit<HadithCubitState> {
     'trmizi.json': "جامع الترمذي"
   };
 
-  HadithCubitCubit() : super(HadithCubitInitial());
+  final Map<String, bool> downloadStatus = {};
+
+  HadithCubitCubit() : super(HadithCubitInitial()) {
+    // Initialize downloadStatus when the cubit is created
+    _initializeDownloadStatus();
+  }
+
+  Future<void> _initializeDownloadStatus() async {
+    for (var file in hadithFilesName.keys) {
+      final statusResult = await getIT<CheckHadithStatus>().call(file);
+      statusResult.fold(
+        (failure) => downloadStatus[file] = false,
+        (isDownloaded) => downloadStatus[file] = isDownloaded,
+      );
+    }
+  }
 
   Future<void> loadHadiths(String fileName) async {
     emit(HadithCubitLoading());
@@ -31,30 +48,65 @@ class HadithCubitCubit extends Cubit<HadithCubitState> {
     result
         .fold((failure) => HadithCubitFailure(errMessage: failure.errorMessage),
             (hadiths) async {
-      final downloadStatus = <String, bool>{};
       for (var file in hadithFilesName.keys) {
         // final statusResult = await checkHadithStatus(file);
         final statusResult = await getIT<CheckHadithStatus>().call(file);
         statusResult.fold((failure) => downloadStatus[file] = false,
-            (isDownloaded) => downloadStatus[file] = isDownloaded);
+            (isDownloaded) {
+          downloadStatus[file] = isDownloaded;
+          print('dowloadeddfile---> $isDownloaded');
+        });
       }
       emit(
           HadithCubitSuccess(hadiths: hadiths, downloadStatus: downloadStatus));
     });
   }
 
-  Future<void> toggleHadithDownload(String fileName, String content) async {
-    if (state is! HadithCubitSuccess) return;
+  Future<void> toggleHadithDownload(
+      String fileName, List<HadithEntity> content) async {
+    // Get the current download status
+    final isDownloaded = downloadStatus[fileName] ?? false;
 
-    final currentState = state as HadithCubitSuccess;
-    final isDownloaded = currentState.downloadStatus[fileName] ?? false;
-
+    emit(HadithCubitLoading());
+    // Perform the download or remove operation
     final result = isDownloaded
-        ? await getIT<RemoveHadith>().call(fileName)
-        : await getIT<DownloadFileUseCase>().call(fileName, content);
+        ? await getIT<RemoveHadith>()
+            .call(fileName) // Remove if already downloaded
+        : await getIT<DownloadFileUseCase>()
+            .call(fileName, content); // Download and cache
 
     result.fold(
-        (failure) => emit(HadithCubitFailure(errMessage: failure.errorMessage)),
-        (_) => loadHadiths(fileName));
+      (failure) => emit(HadithCubitFailure(errMessage: failure.errorMessage)),
+      (_) {
+        // Update the downloadStatus map
+        downloadStatus[fileName] = !isDownloaded;
+
+        // Emit the new state with the updated downloadStatus
+        emit(HadithCubitSuccess(
+          hadiths: content, // Pass the actual hadiths if needed
+          downloadStatus: downloadStatus,
+        ));
+      },
+    );
+  }
+
+  Future<void> navigateToHadithContent(
+      String fileName, BuildContext context) async {
+    final result = await getIT<HadithRepo>().getHadiths(fileName);
+
+    result.fold((failure) {
+      // Show an error message if the content is not found
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(failure.errorMessage)),
+      );
+    }, (content) {
+      // Navigate to the content screen with the cached content
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HadithContentScreen(content: content),
+        ),
+      );
+    });
   }
 }
